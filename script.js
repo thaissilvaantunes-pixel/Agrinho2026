321e00c603611ee3f9e6360002f6aab81e628f6e
palomaavilarocha-a11y <paloma.avila.rocha@escola.pr.gov.br>
2026-06-03 11:23:06 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,52 @@
+let nivel = 20;
 
+function alimentar(){
+
+nivel += 25;
+
+if(nivel > 100){
+nivel = 100;
+}
+
+document.getElementById("nivelRacao").style.width =
+nivel + "%";
+
+document.getElementById("statusRacao").innerHTML =
+"Comedouro acionado com sucesso.";
+}
+
+function ativarCompostagem(){
+
+document.getElementById("statusComposto").innerHTML =
+"Processo iniciado. Resíduos sendo transformados em adubo.";
+}
+
+function calcular(){
+
+let animais =
+parseInt(
+document.getElementById("animais").value
+);
+
+if(isNaN(animais)){
+
+document.getElementById("resultado").innerHTML =
+"Digite uma quantidade válida.";
+
+return;
+}
+
+let racao = animais * 2;
+
+let adubo = animais * 3;
+
+document.getElementById("resultado").innerHTML =
+
+`Para ${animais} animais:
+
+• ${racao} kg de ração por dia
+
+• ${adubo} kg de adubo por mês
+
+• Economia estimada de resíduos: ${animais*5}%`;
+}
